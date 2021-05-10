eed81fc9fb62e86e089a9d65202dcfe224dc153d
amiddendorp22 <amiddendorp22@gmail.com>
2021-05-09 17:09:53 -0700

    Added video demo links

--- turnin/amidd001_lab8_part1.c
+++ turnin/amidd001_lab8_part1.c
@@ -1,9 +1,9 @@
-/*	Author: lab
+/*	Author: amidd001
  *  Partner(s) Name: 
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *	Demo Video: https://youtu.be/-DLqn8PmcOo
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
